2eefb79a2f19bebc68a54f0fb47920a148a84033
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-03 14:59:16 +0200

    file: switch it up 8kyu

---
+++ 8kyu/switchItUp.js
@@ -0,0 +1,14 @@
+function switchItUp(number) {
+  return [
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+  ][number];
+}
